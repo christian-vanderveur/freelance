4577756fa1771bf30ba47e14bad85eb6df73638f
Christian <christian.vanderveur@gmail.com>
2018-03-27 09:49:09 -0600

    implemented bootstrap styles applicationwide

--- app/documents/documents.component.js
+++ app/documents/documents.component.js
@@ -12,25 +12,25 @@ var DocumentsComponent = /** @class */ (function () {
         this.pageTitle = "Document Dashboard";
         this.documents = [
             {
-                title: "my First doc",
-                description: "my desc",
-                file_url: "http://google.com",
-                updated_at: "oct 3, 1994",
-                image_url: "http.//google.com"
+                title: "My First Doc",
+                description: 'asdfasdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             },
             {
-                title: "my First doc",
-                description: "my desc",
-                file_url: "http://google.com",
-                updated_at: "oct 3, 1994",
-                image_url: "http.//google.com"
+                title: "My Second Doc",
+                description: 'asdfasdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             },
             {
-                title: "my First doc",
-                description: "my desc",
-                file_url: "http://google.com",
-                updated_at: "oct 3, 1994",
-                image_url: "http.//google.com"
+                title: "My Last Doc",
+                description: 'asdfasdfasdf asdfasd',
+                file_url: 'http://google.com',
+                updated_at: '11/11/16',
+                image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
             }
         ];
     }
@@ -38,7 +38,8 @@ var DocumentsComponent = /** @class */ (function () {
         core_1.Component({
             moduleId: module.id,
             selector: 'documents',
-            templateUrl: 'documents.component.html'
+            templateUrl: 'documents.component.html',
+            styleUrls: ['documents.component.css']
         })
     ], DocumentsComponent);
     return DocumentsComponent;
